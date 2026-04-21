8b9b0ac594a0864649a99860d0fe0ac390086afe
zhangzhilin700-pixel <zhangzhilin700@gmail.com>
2026-04-21 11:37:39 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ background: '#0a2f1f', color: 'white', textAlign: 'center', padding: '50px' }}>
+      <h1>🐋 OrcaNext</h1>
+      <p>架構三 · 監控儀表板</p>
+      <p>錯誤日誌 | 系統健康 | 預測分析</p>
+    </div>
+  );
+}
